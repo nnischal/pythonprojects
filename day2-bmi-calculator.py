723a3f0b2942a1f2fdcb2b0f9e476dd1f53f3428
Nischal Niraula <nischalniroula18@gmail.com>
2023-07-14 12:32:38 +0545

    bmi edit

--- day2-bmi-calculator.py
+++ day2-bmi-calculator.py
@@ -1,3 +1,4 @@
-height = float(input("Enter your height in m:"))
-weight = float(input("Enter the weight in kg:"))
+height = float(input("Enter your height in m: "))
+weight = float(input("Enter the weight in kg: "))
 bmi = round(weight/height ** 2)
+print("Yoour Body Mass Index is " + str(bmi))
