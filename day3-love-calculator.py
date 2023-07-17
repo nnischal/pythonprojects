3b83c335b65b2f9bd8ca486b6a2cb17e3d644ad3
Nischal Niraula <nischalniroula18@gmail.com>
2023-07-17 13:51:11 +0545

    love-calculator

--- day3-love-calculator.py
+++ day3-love-calculator.py
@@ -1,4 +1,4 @@
-print("Welcome to the Love Calculator! ")
+print("Welcome to the Love Calculator!! ")
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
 combined_string = name1 + name2
